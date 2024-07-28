132bb92ab6c33e25dbe753c8f5a70f531bc0e71d
bakseck <seck.bakar@ugb.edu.sn>
2024-07-28 00:06:26 +0000

    commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,13 @@
 const socket = io();
 
+document.getElementById('loading').style.display = 'block';
+document.getElementById('qr-container').style.display = 'none';
+document.getElementById('content').style.display = 'none';
+
 socket.on('qr', (qr) => {
+    document.getElementById('loading').style.display = 'none';
     document.getElementById('qr-container').style.display = 'block';
+    document.getElementById('content').style.display = 'none';
 
     const qrCodeContainer = document.getElementById('qr-code');
     qrCodeContainer.innerHTML = '';
@@ -13,7 +19,9 @@ socket.on('qr', (qr) => {
 });
 
 socket.on('ready', () => {
+    document.getElementById('loading').style.display = 'none';
     document.getElementById('qr-container').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
 });
 
 socket.on('message', (msg) => {
